07a050646c1e9cbe53c5742dc93ef3f303d2d8d5
AndyAtari <andrewsalteri@gmail.com>
2021-06-12 22:34:25 -0400

    Styled headers. Added styling for home page.:

--- app/javascript/components/Image.js
+++ app/javascript/components/Image.js
@@ -1,11 +1,17 @@
 import React from "react"
 
+
 class Image extends React.Component {
+
+     
     
     render() {
         return (
-            <div className="spacestation-img">
-                <img src= "Starfinder SpaceStation.jpg" alt="spacestation" width="100%" height="100%"></img>
+            <div className="background">
+                <h1>Create a Character</h1>
+                <h1>Create an Adventure</h1>
+                <h1>Create a Starship</h1>
+                <h1>Join a Crew</h1>
             </div>
         )
     }
