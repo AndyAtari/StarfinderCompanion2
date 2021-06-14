05278deb08048df3a2c4119ef5f3f4104cc64410
AndyAtari <andrewsalteri@gmail.com>
2021-06-13 20:41:27 -0400

    Added error message and flash messages to shared partials.

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
 
   root 'site#index'
   
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
+  resources :users
 end
