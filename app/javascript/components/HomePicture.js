ae0ec86a931d859db6d6195bb6b7f38877a3d29f
AndyAtari <andrewsalteri@gmail.com>
2021-06-15 13:06:39 -0400

    Built React component that renders picture if user is not logged in.

---
+++ app/javascript/components/HomePicture.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const HomePicture = () => {
+    return (
+        <div className="spacestation-img">
+            <img src= "Starfinder SpaceStation.jpg" alt="spacestation" width="100%" height="100%"></img>
+        </div>
+    )
+}
+
+export default HomePicture;
