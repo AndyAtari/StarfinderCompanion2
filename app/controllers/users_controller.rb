0661a463caf53b3b4320abae303657c1d827268b
AndyAtari <andrewsalteri@gmail.com>
2021-06-14 20:21:18 -0400

    Built create method in User Controller. Added password confirmation.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,19 @@ class UsersController < ApplicationController
     end
 
     def create
-
+        @user = User.new(user_params)
+        if @user.save 
+            flash[:success] = "Welcome to Starfinder 2.0 #{@user.username}"
+            session[:user_id] = @user.id 
+            redirect_to root_url 
+        else
+            render "new"
+        end
     end
 
+    private 
+
+    def user_params
+        params.require(:user).permit(:username, :email, :password, :password_confirmation)
+    end
 end
