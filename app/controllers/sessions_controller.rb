7f71b0d33539e61579102510807177d668064274
AndyAtari <andrewsalteri@gmail.com>
2021-06-15 12:47:34 -0400

    Added Navbar logic to only display links when player is signed in.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,16 +1,16 @@
 class SessionsController < ApplicationController
 
     skip_before_action :verified_user, only: [:new, :create]
-    
+
     def new
     end
 
     def create
-        user = User.find_by(email: params[:session][:email].downcase)
-        if user && user.authenticate(params[:session][:password])
+        user = User.find_by(email: params[:email])
+        if user.try(:authenticate, params[:password])
             session[:user_id] = user.id 
             flash[:success] = "Welcome explorer, You have signed in."
-            redirect_to users_path(user)
+            redirect_to "/"
         else
             flash.now[:danger] = "I am sorry. Our Starfinder AI can not find that login information."
             render "new"
